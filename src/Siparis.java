bc3abeb76f09e44249e5d1e9619df34f8f645eb2
Yasir Dönmez <yasirdonmez12345@gmail.com>
2025-09-28 18:43:19 +0300

    sipariş dosyası yapıldı

---
+++ src/Siparis.java
@@ -0,0 +1,24 @@
+public class Siparis {
+    private int siparisNo;
+    private Musteri musteri;
+
+    public Siparis(int siparisNo) {
+        this.siparisNo = siparisNo;
+    }
+
+    public int getSiparisNo() {
+        return siparisNo;
+    }
+
+    public void setSiparisNo(int siparisNo) {
+        this.siparisNo = siparisNo;
+    }
+
+    public Musteri getMusteri() {
+        return musteri;
+    }
+
+    public void setMusteri(Musteri musteri) {
+        this.musteri = musteri;
+    }
+}
